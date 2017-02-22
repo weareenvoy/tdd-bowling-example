aefa1499f793e84728f544745922893724e0a05d
Jeff Rupert <rupert.jeff@gmail.com>
2017-02-22 11:31:27 -0800

    Refactored BowlingGame::score() to be more readable.

--- src/BowlingGame.php
+++ src/BowlingGame.php
@@ -1,26 +1,68 @@
 <?php
 
+/**
+ * Class BowlingGame
+ */
 class BowlingGame
 {
+    /**
+     * @var array
+     */
     private $rolls = [];
 
+    /**
+     * @param int $pins
+     */
     public function roll(int $pins)
     {
         $this->rolls[] = $pins;
     }
 
+    /**
+     * @return int
+     */
     public function score(): int
     {
         $score = 0;
 
         for ($frameIndex = 0, $i = 0; $frameIndex < 10; $frameIndex++, $i += 2) {
-            if (10 === $this->rolls[$i] + $this->rolls[$i + 1]) {
-                $score += 10 + $this->rolls[$i + 2];
+            if ($this->isFrameSpare($i)) {
+                $score += $this->scoreSpareFrame($i);
             } else {
-                $score += $this->rolls[$i] + $this->rolls[$i + 1];
+                $score += $this->scoreFrame($i);
             }
         }
 
         return $score;
+    }
+
+    /**
+     * @param int $rollIndex
+     *
+     * @return int
+     */
+    private function scoreFrame(int $rollIndex): int
+    {
+        return $this->rolls[$rollIndex] + $this->rolls[$rollIndex + 1];
+    }
+
+    /**
+     * @param int $rollIndex
+     *
+     * @return int
+     */
+    private function scoreSpareFrame(int $rollIndex): int
+    {
+        return 10 + $this->rolls[$rollIndex + 2];
+    }
+
+    /**
+     * @param int $rollIndex
+     *
+     * @return bool
+     */
+    private function isFrameSpare(int $rollIndex): bool
+    {
+        return 10 === $this->scoreFrame($rollIndex);
     }
 }
